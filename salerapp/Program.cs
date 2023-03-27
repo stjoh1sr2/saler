2350ff43988c85085ccf870197910cf811843df2
FreezerBurn53 <ethanhubbell23@gmail.com>
2023-03-27 11:46:00 -0400

    Basic Database template

--- salerapp/Program.cs
+++ salerapp/Program.cs
@@ -1,3 +1,8 @@
+//using System;
+//using Microsoft.Data.SqlClient;
+//using System.Text;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +28,49 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+
+
+// namespace salerapp
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             try 
+//             { 
+//                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+//                 builder.DataSource = "salersqlserver.database.windows.net"; 
+//                 builder.UserID = "saleradmin";            
+//                 builder.Password = "Compsci10!";     
+//                 builder.InitialCatalog = "SalerDatabase";
+
+//                 using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+//                 {
+//                     Console.WriteLine("\nQuery data example:");
+//                     Console.WriteLine("=========================================\n");
+
+//                     String sql = "SELECT name, collation_name FROM sys.databases";
+
+//                     using (SqlCommand command = new SqlCommand(sql, connection))
+//                     {
+//                         connection.Open();
+//                         using (SqlDataReader reader = command.ExecuteReader())
+//                         {
+//                             while (reader.Read())
+//                             {
+//                                 Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+//                             }
+//                         }
+//                     }                    
+//                 }
+//             }
+//             catch (SqlException e)
+//             {
+//                 Console.WriteLine(e.ToString());
+//             }
+//             Console.ReadLine();
+//         }
+//     }
+// }

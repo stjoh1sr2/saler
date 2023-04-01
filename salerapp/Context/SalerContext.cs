aa4e7a1b7fa04d27b4dd09e72e0d556bed6631c8
Samuel St John <stjoh1sr@cmich.edu>
2023-03-31 22:14:45 -0400

    Add database connection context

---
+++ salerapp/Context/SalerContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using salerapp.Models;
+
+namespace salerapp.Context
+{
+    public class SalerContext: DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Listing> Listings { get; set; }
+
+		public SalerContext() { }
+		public SalerContext(DbContextOptions<SalerContext> options): base(options) { }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			var connectionString = configuration.GetConnectionString("SalerContext");
+			optionsBuilder.UseSqlServer(connectionString);
+		}
+	}
+}

c90da3bd15798ba9103698c954640537c2139d6c
Samuel St John <stjoh1sr@cmich.edu>
2023-04-01 14:16:00 -0400

    Allow users to log out

---
+++ salerapp/Controllers/HeaderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using salerapp.Helpers;
+
+namespace salerapp.Controllers
+{
+    public class HeaderController : Controller
+    {
+        public ActionResult LogOut()
+        {
+            UserManagementHelper.LogOut();
+
+            return Redirect("~/");
+        }
+    }
+}

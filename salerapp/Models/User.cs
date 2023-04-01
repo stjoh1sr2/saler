cf1ddfb7f78c9f15d768bb6f24c3750e9a1134f2
Samuel St John <stjoh1sr@cmich.edu>
2023-03-31 22:14:57 -0400

    Add models

---
+++ salerapp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace salerapp.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+
+        [NotMapped]
+        public bool LoggedIn { get; set; } = false;
+    }
+}

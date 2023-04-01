fc59e48a39426bf3569df12d54265d1fe1bb240c
Samuel St John <stjoh1sr@cmich.edu>
2023-03-31 22:15:10 -0400

    Add helpers

---
+++ salerapp/Helpers/EncryptionDecryptionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace salerapp.Helpers
+{
+	public class EncryptionDecryptionHelper
+	{
+		private static string key = "AAECAwQFBgcICQoLDA0ODw==";
+		public static string Encrypt(string plainText)
+		{
+			return Convert.ToBase64String((new AesManaged { Key = Encoding.UTF8.GetBytes(key), Mode = CipherMode.ECB }).CreateEncryptor().TransformFinalBlock(Encoding.UTF8.GetBytes(plainText), 0, Encoding.UTF8.GetBytes(plainText).Length));
+		}
+
+		public static string Decrypt(string cipherText)
+		{
+			return Encoding.UTF8.GetString((new AesManaged { Key = Encoding.UTF8.GetBytes(key), Mode = CipherMode.ECB }).CreateDecryptor().TransformFinalBlock(Convert.FromBase64String(cipherText), 0, Convert.FromBase64String(cipherText).Length));
+		}	
+	}
+}
---
+++ salerapp/Helpers/UserManagementHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using salerapp.Models;
+
+namespace salerapp.Helpers
+{
+    public class UserManagementHelper
+    {
+        public static User GlobalUser { get; set; } = new User();
+
+        public static void LogOut()
+        {
+            GlobalUser = new User();
+        }
+    }
+}

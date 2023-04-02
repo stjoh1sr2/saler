c6df33cff9bd3a8dae846f3efb4d0f082eea97cb
Samuel St John <stjoh1sr@cmich.edu>
2023-04-01 20:19:35 -0400

    Add view my listings page

---
+++ salerapp/Pages/ViewMyListings.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using salerapp.Context;
+using salerapp.Helpers;
+using salerapp.Models;
+
+namespace salerapp.Pages
+{
+    public class ViewMyListingModel: PageModel
+    {
+        public SalerContext db = new SalerContext();
+        public IEnumerable<Listing> listings;
+
+        public void LoadListings()
+        {
+            if (UserManagementHelper.GlobalUser.LoggedIn)
+            {
+                listings = db.Listings.Where(l => l.PosterId == UserManagementHelper.GlobalUser.UserId)
+                    .OrderByDescending(l => l.EndDate);
+            }
+        }
+    }
+}

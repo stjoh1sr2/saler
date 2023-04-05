cb89be775095f95c277d3d7aba4223396f6151f8
Samuel St John <stjoh1sr@cmich.edu>
2023-04-05 12:12:07 -0400

    Individual pages for listings

---
+++ salerapp/Pages/Listing.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using salerapp.Context;
+using salerapp.Helpers;
+using salerapp.Models;
+
+namespace salerapp.Pages
+{
+    public class ListingModel: PageModel
+    {
+        public SalerContext db = new SalerContext();
+
+        public Listing LoadListing(int id)
+        {
+            return db.Listings.Where(l => l.ListingId == id).SingleOrDefault();
+		}
+
+        public string LoadUserName(int id)
+        {
+            User tempUser = db.Users.Where(u => u.UserId == id).SingleOrDefault();
+
+            return (tempUser is not null) ? tempUser.UserName : "Unknown";
+        }
+    }
+}

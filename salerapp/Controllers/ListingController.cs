3ca7bc4d9b5489d189f3913b4cf1898731048781
Samuel St John <stjoh1sr@cmich.edu>
2023-04-16 20:03:43 -0400

    Add ability to save listings

--- salerapp/Controllers/ListingController.cs
+++ salerapp/Controllers/ListingController.cs
@@ -28,5 +28,52 @@ namespace salerapp.Controllers
 
             return Redirect(HttpContext.Request.Headers["Referer"].ToString());
         }
+
+        public IActionResult SaveListing(int listingId)
+        {
+            // Make sure a user is logged in
+            if (HttpContext.Session.GetString("_User") is not null)
+            {
+                int userId = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("_User")).UserId;
+
+                // Only add new saved listings
+                if (db.SavedListings.Where(s => s.UserId == userId && s.ListingId == listingId).Count() <= 0)
+                {
+                    SavedListing tempSaveListing = new SavedListing
+                    {
+                        ListingId = listingId,
+                        UserId = userId,
+                        SavedTime = DateTime.Now
+                    };
+
+                    // Add new listing to database
+                    db.SavedListings.Add(tempSaveListing);
+                    db.SaveChanges();
+                }
+            }
+
+            // Redirect to current page
+            return Redirect(HttpContext.Request.Headers["Referer"].ToString());
+        }
+
+        public IActionResult UnsaveListing(int listingId)
+        {
+            // Make sure a user is logged in
+            if (HttpContext.Session.GetString("_User") is not null)
+            {
+                int userId = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("_User")).UserId;
+
+                // Make sure currently saved user has this listing saved
+                if (db.SavedListings.Where(s => s.UserId == userId && s.ListingId == listingId).Count() >= 1)
+                {
+                    // Remove listing
+                    db.SavedListings.Remove(db.SavedListings.Where(s => s.UserId == userId && s.ListingId == listingId).FirstOrDefault());
+                    db.SaveChanges();
+                }
+            }
+
+            // Redirect to current page
+            return Redirect(HttpContext.Request.Headers["Referer"].ToString());
+        }
     }
 }
